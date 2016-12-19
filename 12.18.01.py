db5dfb1984f53d2ebf2e49c634b9e61a76208387
CentOS7 <apktoolOo@gmail.com>
2016-12-19 22:30:39 +0800

    if,else

---
+++ 12.18.01.py
@@ -0,0 +1,9 @@
+#--coding:utf-8--
+print("--------------hello world----------------")
+temp=input("Couldn't input something?")
+guess=int(temp)
+if guess==8:
+	print("Yes, it is true")
+else:
+	print("No, it is wrong")
+print("The end")
