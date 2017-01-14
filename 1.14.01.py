8a3227f4ee6c9da38a1774d0bd0192055b838c72
CentOS7 <apktoolOo@gmail.com>
2017-01-14 21:35:28 +0800

    函数可以以list的形式返回多个值

---
+++ 1.14.01.py
@@ -0,0 +1,15 @@
+# 函数可以以list的形式返回多个值
+
+def MyFunction():
+	return [1,'2',(3,),[4,5]]
+
+temp=MyFunction();
+print(temp)
+
+'''
+函数（function）：有返回值
+过程（procedure）：是简单，特殊并且没有返回值的
+'''
+'''
+区部变量和全局变量
+'''
