46120bb6e85725fb33bb66303a9eccc6bf5adc52
CentOS7 <apktoolOo@gmail.com>
2017-01-15 11:17:39 +0800

    函数|递归(factorial,Fibonacci)

--- 1.15.03.py
+++ 1.15.03.py
@@ -1,5 +1,6 @@
-# 函数|递归
+# 函数|递归(factorial,Fibonacci)
 
+# 求解factorial
 def MyFunction(n):
 	if n==1:
 		return 1
@@ -7,3 +8,11 @@ def MyFunction(n):
 
 temp=MyFunction(5)
 print(temp)
+
+# 求解Fibonacci sequence
+def Feber(x):
+	if x==1 or x==2:
+		return 1
+	return Feber(x-1)+Feber(x-2)
+
+print(Feber(20))
