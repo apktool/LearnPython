59063c1a20013787fd9bbc1a1c9af62ff4d43cea
apktool <apktoolOo@gmail.com>
2017-01-22 12:37:06 +0800

    参数解析|argparse|group

--- 1.22.02.py
+++ 1.22.02.py
@@ -1,22 +1,36 @@
-# 参数解析|argparse
+# 参数解析|argparse|group
 
 import argparse
 
 parser=argparse.ArgumentParser(prog='myProgram',description='It will show something') # --help 会输出description部分；prog选项则决定help中输出的不是文件名，而是定义的名称
-parser.add_argument('--run', action='store',
+
+group1=parser.add_argument_group('MyFistGroup')
+group1.add_argument('--run', action='store',
                     dest='value', # 将输入的参数绑定到value上，比如输入下述指令，则有value=rerun
-                    help='just a test')
+                    help='just test rerun')
+group1.add_argument('--walk', action='store',
+                    dest='rewalk',
+                    help='just test walk')
+
+group2=parser.add_argument_group('MyScondGroup')
+group2.add_argument('--dog', action='store_true',
+                    dest='dog',
+                    help='just test dog')
+group2.add_argument('--cat', action='store_false',
+                    dest='cat',
+                    help='just test cat')
 
 args=parser.parse_args()
-# print(args)
+print(args)
 # print(args.value)
 
 def fun_dest(value):
-    print(value)
+    print('->'+value)
 
 if args.value=='rerun':
     fun_dest(args.value)
 
+# python3 1.22.02.py --help
 # python3 1.22.02.py --run rerun
 
 '''
