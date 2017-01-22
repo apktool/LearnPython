96c69f6aa74c32d91fccd7ffde25b2c339883d5c
apktool <apktoolOo@gmail.com>
2017-01-22 11:35:36 +0800

    参数解析|argparse

---
+++ 1.22.02.py
@@ -0,0 +1,35 @@
+# 参数解析|argparse
+
+import argparse
+
+parser=argparse.ArgumentParser(prog='myProgram',description='It will show something') # --help 会输出description部分；prog选项则决定help中输出的不是文件名，而是定义的名称
+parser.add_argument('--run', action='store',
+                    dest='value', # 将输入的参数绑定到value上，比如输入下述指令，则有value=rerun
+                    help='just a test')
+
+args=parser.parse_args()
+# print(args)
+# print(args.value)
+
+def fun_dest(value):
+    print(value)
+
+if args.value=='rerun':
+    fun_dest(args.value)
+
+# python3 1.22.02.py --run rerun
+
+'''
+ArgumentParser.add_argument(name or flags...[, action][, nargs][, const][, default][, type][, choices][, required][, help][, metavar][, dest])
+    name or flags - Either a name or a list of option strings, e.g. foo or -f, --foo.
+    action - The basic type of action to be taken when this argument is encountered at the command line.
+    nargs - The number of command-line arguments that should be consumed.
+    const - A constant value required by some action and nargs selections.
+    default - The value produced if the argument is absent from the command line.
+    type - The type to which the command-line argument should be converted.
+    choices - A container of the allowable values for the argument.
+    required - Whether or not the command-line option may be omitted (optionals only).
+    help - A brief description of what the argument does.
+    metavar - A name for the argument in usage messages.
+    dest - The name of the attribute to be added to the object returned by parse_args().
+'''
