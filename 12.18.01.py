3a337fe7b1922504e82a9e99f47027d4e2ef2829
apktool <apktoolOo@gmail.com>
2017-02-06 16:53:59 +0800

    print实现简单的格式控制

--- 12.18.01.py
+++ 12.18.01.py
@@ -7,3 +7,13 @@ if guess==8:
 else:
 	print("No, it is wrong")
 print("The end")
+
+
+str1='hello world'
+print('%s'%(str1)+'|')
+print('%20s'%(str1)+'|')
+print('%-20s'%(str1)+'|')
+
+'''
+print实现简单的格式控制
+'''
