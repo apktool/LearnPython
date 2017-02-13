f5cf09edb1b38b54beb677829792200ffd1a59b4
apktool <apktoolOo@gmail.com>
2017-02-13 13:48:43 +0800

    socket|server|tcp

--- 2.12.02.py
+++ 2.12.02.py
@@ -1,18 +1,16 @@
-# socket|server
+# socket|server|tcp
 
 import socket
 
 serversocket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+serversocket.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
 serversocket.bind(('127.0.0.1',9999))
 serversocket.listen(5)
 
-conn,addr=serversocket.accept()
+(conn,addr)=serversocket.accept()
 
-while True:
-    data = conn.recv(1024)
-    if not data:
-        break
-    print('server received data:%s', data)
-    conn.send(data)
+data = conn.recv(1024)
+print('server received data:%s'%(data))
+conn.send(data)
 
 serversocket.close()
