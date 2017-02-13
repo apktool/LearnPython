02cec7e7fb37fc3646f5205b38a77bab5e44f4ff
apktool <apktoolOo@gmail.com>
2017-02-13 11:00:37 +0800

    socket|client

---
+++ 2.12.03.py
@@ -0,0 +1,10 @@
+# socket|client
+
+import socket
+
+s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.connect(('127.0.0.1',9999))
+s.send(b'hello world')
+data=s.recv(1024)
+
+print('client receive data %s:'%(data))
