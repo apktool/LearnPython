7cb04629b3fb73b04a9fcc8ce6c1b687e675170f
apktool <apktoolOo@gmail.com>
2017-03-01 09:32:37 +0800

    多进程|multiprocessing

--- 2.28.01.py
+++ 2.28.01.py
@@ -3,17 +3,35 @@
 from multiprocessing import Pool
 
 def f1(x):
-	return x*x
+    return x*x
 
 with Pool(5) as p:
-	print(p.map(f1,[1,2,3]))
+    print(p.map(f1,[1,2,3]))
 
 #---------------------------------
+print('---------------')
+#---------------------------------
+
+import multiprocessing
+from multiprocessing import Pool as ThreadPool
+
+def f2(x):
+    print('%s -> %d'%(multiprocessing.current_process().name,x*x))
+
+p=ThreadPool()
+p.map(f2,range(10))
+p.close()
+p.join()
+
+#---------------------------------
+print('---------------')
+#---------------------------------
+
 from multiprocessing import Process
 
-def f2(name):
-	print('hello %s'%(name))
+def f3(name):
+    print('hello %s'%(name))
 
-p=Process(target=f2,args=('Alice',))
+p=Process(target=f3,args=('Alice',))
 p.start()
 p.join()
